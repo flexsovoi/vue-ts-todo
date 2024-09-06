6e45283e58d71c6640515d14bf04f76e72bac416
Danil Belous <flexsovoi@yandex.ru>
2024-09-06 09:59:43 +0300

    feat(TaskForm): add test

--- src/widgets/TaskForm/__tests__/TaskForm.test.ts
+++ src/widgets/TaskForm/__tests__/TaskForm.test.ts
@@ -1,11 +1,28 @@
-import { describe, vi } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { createPinia, setActivePinia } from 'pinia';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { useTaskStore } from '../../../entities/task/store/TaskStore';
+import TaskForm from '../TaskForm.vue';
 
 describe('Task Form', () => {
-  const mockEvent = {
-    preventDefault: vi.fn(),
-    target: {
-      taskName: 'Test Task',
-      taskLevel: 'HIGH',
-    },
-  } as unknown as Event;
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('task form work correctly', async () => {
+    const store = useTaskStore();
+    store.setCreateMode(true);
+    const wrapper = mount(TaskForm);
+    const taskForm = wrapper.findComponent(TaskForm);
+    expect(taskForm.exists()).toBe(true);
+
+    await wrapper.find('input[name="taskName"]').setValue('Test Task');
+    await wrapper.find('select[name="taskLevel"]').setValue('HIGH');
+
+    await wrapper.find('form').trigger('submit.prevent');
+    expect(store.isCreateMode).toEqual(false);
+    expect(!!store.tasks.find((task) => task.title === 'Test Task')).toEqual(
+      true,
+    );
+  });
 });
